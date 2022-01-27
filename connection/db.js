6a50124efb5f0729a3c3bebac30280a9a06269b5
unknown <hamamullaiqi@gmail.com>
2022-01-27 17:00:01 +0700

    move database

--- connection/db.js
+++ connection/db.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require('pg')
 
     const connectionString = 'postgres://zgcnminxzgzaih:d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c@ec2-23-20-73-25.compute-1.amazonaws.com:5432/deoin7b7vl87m'
 
