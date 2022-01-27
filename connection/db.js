216f9c46a41985d00ec229f743001913e08f161b
unknown <hamamullaiqi@gmail.com>
2022-01-27 16:52:52 +0700

    move database

--- connection/db.js
+++ connection/db.js
@@ -1,13 +1,9 @@
 const { Pool } = require('pg')
 
-    // const connectionString = 'postgres://zgcnminxzgzaih:d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c@ec2-23-20-73-25.compute-1.amazonaws.com:5432/deoin7b7vl87m'
+    const connectionString = 'postgres://zgcnminxzgzaih:d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c@ec2-23-20-73-25.compute-1.amazonaws.com:5432/deoin7b7vl87m'
 
     const dbPool = new Pool({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        port: process.env.POSTGRES_PORT,
+        connectionString: connectionString,
     })
 
 
