a90e623603b381161724c0db74d9a0d054c9342f
unknown <hamamullaiqi@gmail.com>
2022-01-27 17:13:26 +0700

    move database

--- connection/db.js
+++ connection/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg')
 
-    // const connectionString = 'postgres://zgcnminxzgzaih:d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c@ec2-23-20-73-25.compute-1.amazonaws.com:5432/deoin7b7vl87m'
+    const connectionString = 'postgres://zgcnminxzgzaih:d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c@ec2-23-20-73-25.compute-1.amazonaws.com:5432/deoin7b7vl87m'
 
-    // const dbPool = new Pool({
-    //     connectionString: connectionString,
-    // })
+    const dbPool = new Pool({
+        connectionString: connectionString,
+        ssl: { rejectUnauthorized: false }
+    })
 
 
 // const dbPool = new Pool ({
@@ -14,14 +15,14 @@ const { Pool } = require('pg')
 //     password : 'admin'
 // })
 
-var dbPool = new Pool({
-    user: "zgcnminxzgzaih",
-    password: "d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c",
-    database: "deoin7b7vl87m",
-    port: 5432,
-    host: "ec2-23-20-73-25.compute-1.amazonaws.com",
-    ssl: true
-});
+// var dbPool = new Pool({
+//     user: "zgcnminxzgzaih",
+//     password: "d6a994a39b6902c0f7e68e7ea897a6f6262ba1c671e130ad5962db5a556ff22c",
+//     database: "deoin7b7vl87m",
+//     port: 5432,
+//     host: "ec2-23-20-73-25.compute-1.amazonaws.com",
+//     ssl: true
+// });
 
 
 module.exports = dbPool
